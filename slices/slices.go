e0ecafb5a510e98cde285610e1974dd4301dcd3d
Sarthak412 <sarthakkamble412@gmail.com>
2026-06-22 22:08:18 +0530

    Slices in golang

--- slices/slices.go
+++ slices/slices.go
@@ -5,10 +5,35 @@ import "fmt"
 func main() {
 
 	// * Slices in Golang
-	arr := [5]int{1, 2, 3, 4, 5}
+	// arr := []int{1, 2, 3, 4, 5}
 
-	sl := arr[:3]
+	// fmt.Println(len(arr)) // Length -> 5
+	
+	// Let's find the Data Type of the arr
+	// fmt.Printf("%T\n", arr)
+	
+	// // ? Append Method in Slices
+	
+	// arr = append(arr, 6, 7, 8)
+	// fmt.Println(arr)
+	
+	// fmt.Println("Length: ", len(arr)) 
 
-	fmt.Println(sl)
+	// ? make function
+
+	// skills := []string{}   // ? Normal Declaration
+
+	skills := make([]string, 3, 5) // ? make(type, length, capacity)
+
+	skills = append(skills, "C")
+	skills = append(skills, "C++")
+	skills = append(skills, "Golang")
+	skills = append(skills, "JavaScript")
+	skills = append(skills, "TypeScript")
+
+
+	fmt.Println("Slice: ", skills)
+	fmt.Println("Length: ", len(skills))
+	fmt.Println("Capacity: ", cap(skills))
 
 }
